22f03a9aa859fc0e162a40f29f52247ef51f5dee
Дмитрий <safonovdmitr@gmail.com>
2018-11-09 15:48:38 +0300

    refactoring code

--- database.rb
+++ database.rb
@@ -1,15 +1,19 @@
-require 'pg'
-require 'dotenv/load'
+class Database
+  require 'pg'
+  require 'dotenv/load'
 
-def connect_to_db
-  PG.connect(host: ENV['DB_HOST'],
-    port: ENV['DB_PORT'],
-    user: ENV['DB_USERNAME'],
-    password: ENV['DB_PASSWORD'],
-    dbname: ENV['DB_NAME'])
-end
+  attr_reader :db
 
-def write_to_db(table)
-  value = table.map { |t| "('#{t[0]}', #{t[1]}, #{t[2]})" }.join(',')
-  connect_to_db.exec("INSERT INTO shopping_list (name, count, price) VALUES #{value}")
+  def initialize
+    @db = PG.connect(host: ENV['DB_HOST'],
+      port: ENV['DB_PORT'],
+      user: ENV['DB_USERNAME'],
+      password: ENV['DB_PASSWORD'],
+      dbname: ENV['DB_NAME'])
+  end
+
+  def write_to_db(table)
+    value = table.map { |t| "('#{t[0]}', #{t[1]}, #{t[2]})" }.join(',')
+    @db.exec("INSERT INTO shopping_list (name, count, price) VALUES #{value}")
+  end
 end
--- site.rb
+++ site.rb
@@ -1,10 +1,12 @@
-require 'selenium-webdriver'
+class Site
+  require 'selenium-webdriver'
 
-def load_table
-  driver = Selenium::WebDriver.for :chrome
+  def load_table
+    driver = Selenium::WebDriver.for :chrome
 
-  driver.get('http://tereshkova.test.kavichki.com/')
+    driver.get('http://tereshkova.test.kavichki.com/')
 
-  table = driver.find_elements(tag_name: :tr).map{ |element| element.find_elements(tag_name: :td).map(&:text) }
-  table.delete_if(&:empty?)
+    table = driver.find_elements(tag_name: :tr).map{ |element| element.find_elements(tag_name: :td).map(&:text) }
+    table.delete_if(&:empty?)
+  end
 end
