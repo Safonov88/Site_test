40d566f1d933e9ed366c7979165d732ea0cfe330
Дмитрий <safonovdmitr@gmail.com>
2018-11-10 18:41:05 +0300

    add methods add_to_table and clear_text

--- site.rb
+++ site.rb
@@ -11,4 +11,24 @@ class Site
     table = @driver.find_elements(tag_name: :tr).map{ |element| element.find_elements(tag_name: :td).map(&:text) }
     table.delete_if(&:empty?)
   end
+
+  def add_to_table(values)
+    @driver.find_element(id: :open).click
+
+    values.each do |value|
+      @driver.find_element(id: :name).send_keys (value['name'])
+      @driver.find_element(id: :count).send_keys (value['count'])
+      @driver.find_element(id: :price).send_keys (value['price'])
+
+      @driver.find_element(id: :add).click
+
+      clear_text
+    end
+  end
+
+  def clear_text
+    @driver.find_element(id: :name).clear
+    @driver.find_element(id: :count).clear
+    @driver.find_element(id: :price).clear
+  end
 end
