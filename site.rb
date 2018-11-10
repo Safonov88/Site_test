8242f9f8eebb3936f954a2b43ec70b7134416025
Дмитрий <safonovdmitr@gmail.com>
2018-11-10 16:44:15 +0300

    fix class Site

--- site.rb
+++ site.rb
@@ -1,12 +1,14 @@
 class Site
   require 'selenium-webdriver'
 
+  def initialize
+    @driver = Selenium::WebDriver.for :chrome
+
+    @driver.get('http://tereshkova.test.kavichki.com/')
+  end
+
   def load_table
-    driver = Selenium::WebDriver.for :chrome
-
-    driver.get('http://tereshkova.test.kavichki.com/')
-
-    table = driver.find_elements(tag_name: :tr).map{ |element| element.find_elements(tag_name: :td).map(&:text) }
+    table = @driver.find_elements(tag_name: :tr).map{ |element| element.find_elements(tag_name: :td).map(&:text) }
     table.delete_if(&:empty?)
   end
 end
