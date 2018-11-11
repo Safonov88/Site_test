33112b2e7f4e92c53b6282f687aff0e6cfd801c8
Дмитрий <safonovdmitr@gmail.com>
2018-11-11 22:33:52 +0300

    fix code style to Database

--- database.rb
+++ database.rb
@@ -3,11 +3,14 @@ require 'dotenv/load'
 
 class Database
   def initialize
-    @db = PG.connect(host: ENV['DB_HOST'],
-      port: ENV['DB_PORT'],
-      user: ENV['DB_USERNAME'],
-      password: ENV['DB_PASSWORD'],
-      dbname: ENV['DB_NAME'])
+    @db =
+      PG.connect(
+        host: ENV['DB_HOST'],
+        port: ENV['DB_PORT'],
+        user: ENV['DB_USERNAME'],
+        password: ENV['DB_PASSWORD'],
+        dbname: ENV['DB_NAME']
+      )
   end
 
   def write(shoping_list)
