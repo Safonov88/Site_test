2430410499636f0989add93460b90577acdec0a6
Дмитрий <safonovdmitr@gmail.com>
2018-11-11 23:29:42 +0300

    fix tests

--- spec/test_case_spec.rb
+++ spec/test_case_spec.rb
@@ -40,12 +40,17 @@ describe 'UI site testing' do
       row_number_two = driver.find_element(xpath: "//tr[2]").text
       row_number_three = driver.find_element(xpath: "//tr[3]").text
 
-      expect{ driver.find_element(xpath: '//tr[2]/td[4]').click }.to change{ driver.find_element(xpath: "//tr[2]").text }.from(row_number_two).to(row_number_three)
+      expect{ driver.find_element(xpath: '//tr[2]/td[4]').click }
+        .to change{ driver.find_element(xpath: "//tr[2]").text }
+        .from(row_number_two).to(row_number_three)
     end
   end
 end
 
 describe 'Form' do
+  let(:value) { [{ 'name' => '', 'count' => '', 'price' => '' }] }
+  let(:driver) { Selenium::WebDriver.for :chrome }
+
   context 'when submited' do
     before(:each) do
       driver.get(URL)
@@ -83,19 +88,25 @@ describe 'Form' do
     it 'clears name text field' do
       driver.find_element(id: :name).send_keys('testname')
 
-      expect{ driver.find_element(xpath: "//body/a[2]").click }.to change{ driver.find_element(id: :name).attribute('value') }.from('testname').to be_empty
+      expect{ driver.find_element(xpath: "//body/a[2]").click }
+        .to change{ driver.find_element(id: :name).attribute('value') }
+        .from('testname').to be_empty
     end
 
     it 'clears count text field' do
       driver.find_element(id: :count).send_keys(123456789)
 
-      expect{ driver.find_element(xpath: "//body/a[2]").click }.to change{ driver.find_element(id: :count).attribute('value') }.from(123456789).to be_empty
+      expect{ driver.find_element(xpath: "//body/a[2]").click }
+        .to change{ driver.find_element(id: :count).attribute('value') }
+        .from(123456789).to be_empty
     end
 
     it 'clears price text field' do
       driver.find_element(id: :price).send_keys(123456789.98)
 
-      expect{ driver.find_element(xpath: "//body/a[2]").click }.to change{ driver.find_element(id: :price).attribute('value') }.from(123456789.98).to be_empty
+      expect{ driver.find_element(xpath: "//body/a[2]").click }
+        .to change{ driver.find_element(id: :price).attribute('value') }
+        .from(123456789.98).to be_empty
     end
   end
 end
