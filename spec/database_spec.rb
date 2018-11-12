9d02c2bd5900cc566d948e7ae16902c1505d3b07
Дмитрий <safonovdmitr@gmail.com>
2018-11-12 13:10:10 +0300

    add clear db before each test

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -5,13 +5,16 @@ require_relative '../database'
 
 describe 'Database' do
   let(:db) { Database.new }
-  let(:pg) {PG.connect(
-              host: ENV['DB_HOST'],
-              port: ENV['DB_PORT'],
-              user: ENV['DB_USERNAME'],
-              password: ENV['DB_PASSWORD'],
-              dbname: ENV['DB_NAME']
-            )}
+  let(:pg) { PG.connect(
+               host: ENV['DB_HOST'],
+               port: ENV['DB_PORT'],
+               user: ENV['DB_USERNAME'],
+               password: ENV['DB_PASSWORD'],
+               dbname: ENV['DB_NAME']
+              )
+            }
+
+  before(:each) { pg.exec('TRUNCATE TABLE shopping_list') }
 
   it 'checks write in db' do
     value = [['testname', '123456789', '1234.98']]
@@ -28,5 +31,12 @@ describe 'Database' do
     expect{ db.clear_table }
       .to change{ pg.exec("SELECT * from shopping_list").any? }
       .from(be_truthy).to(be_falsey)
+  end
+
+  it 'checks read from db' do
+    pg.exec("INSERT INTO shopping_list (name, count, price)
+              VALUES ('testname1', 123456789, 1234.98), ('testname2', 123456789, 1234.98)")
+
+    expect(db.read.count).to eq(2)
   end
 end
