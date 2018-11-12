b85344c1454e90ed6373326e935c4472f9e079b6
Дмитрий <safonovdmitr@gmail.com>
2018-11-12 11:05:50 +0300

    fix site_spec

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -4,6 +4,8 @@ require_relative '../site'
 describe 'Site' do
   let(:site) { Site.new(Selenium::WebDriver.for :chrome) }
 
+  after(:each) { site.driver.quit }
+
   it 'returns table' do
     expect(site.load_table).not_to eq([])
   end
@@ -14,5 +16,24 @@ describe 'Site' do
     site.add_to_table(value)
 
     expect(site.driver.find_element(xpath: '//tbody/tr[5]')).to be
+  end
+
+  it 'clears text fields in form' do
+    site.driver.find_element(id: :open).click
+
+    name = site.driver.find_element(id: :name)
+    name.send_keys('testname')
+
+    count = site.driver.find_element(id: :count)
+    count.send_keys(123456789)
+
+    price = site.driver.find_element(id: :price)
+    price.send_keys(123456789.98)
+
+    site.send(:reset_form)
+
+    expect(name.text).to be_empty
+    expect(count.text).to be_empty
+    expect(price.text).to be_empty
   end
 end
