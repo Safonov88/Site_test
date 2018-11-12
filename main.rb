ed32a04c64b856c39a1e3379f94aae7063258f52
Дмитрий <safonovdmitr@gmail.com>
2018-11-12 14:03:11 +0300

    fix main.rb

--- main.rb
+++ main.rb
@@ -4,4 +4,15 @@ require_relative 'site'
 db = Database.new
 site = Site.new(Selenium::WebDriver.for :chrome)
 
+db.clear_table
+
 db.write(site.load_table)
+
+value = [['testname1', '123456789', '1234.98'], ['testname2', '123456789', '1234.98']]
+db.write(value)
+
+table_from_bd = db.read
+
+4.times { site.driver.find_element(xpath: '//tbody/tr[1]/td[4]/a').click }
+
+site.add_to_table(table_from_bd)
